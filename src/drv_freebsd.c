343ffbc7c1d585446c0cd46a5454e4151392488c
Sean Bruno <sbruno@freebsd.org>
2012-07-03 12:06:54 -0700

    Update copyright information to include Hiren and add copyright to dutil_freebsd.h

--- src/drv_freebsd.c
+++ src/drv_freebsd.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012, Sean Bruno sbruno@freebsd.org
+ * Copyright (c) 2012, Hiren Panchasara hiren.panchasara@gmail.com
  * All rights reserved.
 
  * Redistribution and use in source and binary forms, with or without
